cbd18e4020d95eed07b81818235b74c928a75133
taiarima <tai.arima@colorado.edu>
2023-05-31 16:35:33 +0300

    Still building basics.

---
+++ domSelectors.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Selecting elements
+const aboutContainer = document.querySelector(".about-container");
+const sectionTitle = document.querySelector(".section-title");
+const learningContainer = document.querySelector(".learning-container");
+const prompt = document.querySelector(".prompt");
+const userTextContainer = document.querySelector(".user-text-container");
+const userText = document.querySelector(".user-text");
+const keyboardContainer = document.querySelector(".keyboard-container");
+const keyboard = document.querySelector(".keyboard");
+const keys = document.querySelectorAll(".key");
+const keyRowTop = document.querySelectorAll(".row-top");
+const keyRowMiddle = document.querySelectorAll(".row-middle");
+const keyRowBottom = document.querySelectorAll(".row-bottom");
+const keyFiller = document.querySelectorAll(".filler");
+const keyUpperRight = document.querySelectorAll(".key-upper-right");
+
+const keyBieup = document.querySelector(".key.ㅂ");
+const keyJieut = document.querySelector(".key.ㅈ");
+const keyDigeut = document.querySelector(".key.ㄷ");
+const keyGiyeok = document.querySelector(".key.ㄱ");
+const keySiot = document.querySelector(".key.ㅅ");
+const keyYeo = document.querySelector(".key.ㅕ");
+const keyYa = document.querySelector(".key.ㅑ");
+const keyAe = document.querySelector(".key.ㅐ");
+const keyE = document.querySelector(".key.ㅔ");
+const keyMieum = document.querySelector(".key.ㅁ");
+const keyNieun = document.querySelector(".key.ㄴ");
+const keyIeung = document.querySelector(".key.ㅇ");
+const keyRieul = document.querySelector(".key.ㄹ");
+const keyHieuh = document.querySelector(".key.ㅎ");
+const keyEo = document.querySelector(".key.ㅓ");
+const keyA = document.querySelector(".key.ㅏ");
+const keyI = document.querySelector(".key.ㅣ");
+const keyKieuk = document.querySelector(".key.ㅋ");
+const keyTieut = document.querySelector(".key.ㅌ");
+const keyChieut = document.querySelector(".key.ㅊ");
+const keyPieup = document.querySelector(".key.ㅍ");
+const keyU = document.querySelector(".key.ㅜ");
+const keyEu = document.querySelector(".key.ㅡ");
+
+
